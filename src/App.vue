946ed8b0085185dae0c1adeb95907e97bf00e036
nmorduch <1051450+nmorduch@users.noreply.github.com>
2025-01-31 16:52:50 +0000

    Allow sorting countries asc or desc by column
    Set initial sort to 'Total engergy supply', descending

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,34 @@
 <script setup>
+import { ref } from 'vue'
 import Country from './components/Country.vue'
 import data from './assets/data.json'
+
+const countries = ref([...data])
+sortCountries()
+
+function sortCountries(property = 'Total energy supply', ascending = false) {
+  countries.value.sort((a, b) => {
+    let propertyA = a[property]
+    let propertyB = b[property]
+    if (ascending) {
+      if (propertyA < propertyB) {
+        return -1
+      }
+      if (propertyA > propertyB) {
+        return 1
+      }
+      return 0
+    } else {
+      if (propertyA < propertyB) {
+        return 1
+      }
+      if (propertyA > propertyB) {
+        return -1
+      }
+      return 0
+    }
+  })
+}
 </script>
 
 <template>
@@ -12,15 +40,27 @@ import data from './assets/data.json'
     <table>
       <thead>
         <tr>
-          <th>Country</th>
-          <th>Region</th>
-          <th>Total energy supply</th>
+          <th>
+            Country
+            <button @click="sortCountries('country', (ascending = true))">asc</button>
+            <button @click="sortCountries('country', (ascending = false))">desc</button>
+          </th>
+          <th>
+            Region
+            <button @click="sortCountries('region', (ascending = true))">asc</button>
+            <button @click="sortCountries('region', (ascending = false))">desc</button>
+          </th>
+          <th>
+            Total energy supply
+            <button @click="sortCountries('Total energy supply', (ascending = true))">asc</button>
+            <button @click="sortCountries('Total energy supply', (ascending = false))">desc</button>
+          </th>
           <th>Member</th>
         </tr>
       </thead>
       <tbody>
         <Country
-          v-for="country in data"
+          v-for="country in countries"
           :key="country.country"
           :countryName="country.country"
           :region="country.region"
