nmorduch/iea-test

Replace default content with basic table and header

src/components/Country.vue
@@ -1,0 +1,29 @@
+<script setup>
+defineProps({
+  countryName: {
+    type: String,
+    required: true,
+  },
+  region: {
+    type: String,
+    required: true,
+  },
+  totalEnergySupply: {
+    type: [Number, String],
+    default: '—',
+  },
+  IEAMember: {
+    type: Boolean,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <tr>
+    <th>{{ countryName }}</th>
+    <td>{{ region }}</td>
+    <td>{{ totalEnergySupply }}</td>
+    <td>{{ IEAMember }}</td>
+  </tr>
+</template>

nmorduch/table-test

src/components/Country.vue
@@ -1,0 +1,29 @@
+<script setup>
+defineProps({
+  countryName: {
+    type: String,
+    required: true,
+  },
+  region: {
+    type: String,
+    required: true,
+  },
+  totalEnergySupply: {
+    type: [Number, String],
+    default: '—',
+  },
+  member: {
+    type: Boolean,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <tr>
+    <th>{{ countryName }}</th>
+    <td>{{ region }}</td>
+    <td>{{ totalEnergySupply }}</td>
+    <td>{{ member }}</td>
+  </tr>
+</template>